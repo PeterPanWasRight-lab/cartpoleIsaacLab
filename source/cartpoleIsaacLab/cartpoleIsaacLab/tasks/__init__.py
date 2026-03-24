0625009e647a16a18f30b39012c7754e355bf03e
PeterPanWasRight-lab <107916107+PeterPanWasRight-lab@users.noreply.github.com>
2026-03-24 21:10:20 +0800

    增加注释

--- source/cartpoleIsaacLab/cartpoleIsaacLab/tasks/__init__.py
+++ source/cartpoleIsaacLab/cartpoleIsaacLab/tasks/__init__.py
@@ -15,3 +15,6 @@ from isaaclab_tasks.utils import import_packages
 _BLACKLIST_PKGS = ["utils", ".mdp"]
 # Import all configs in this package
 import_packages(__name__, _BLACKLIST_PKGS)
+## import_packages 会自动扫描当前包目录下的所有子模块（除了黑名单中的 utils 和 .mdp）
+# 并递归导入它们的 __init__.py 文件，从而触发这些子模块中的 gym.register() 调用
+# 最终将所有自定义的 Gym 环境注册到 gymnasium 的注册表中，使其可以通过 gym.make() 创建
